04255d04f19e4b05291b5166585ad1e7f91ab50f
SyedWasiHaider <wasih@wasih-mac.local>
2016-07-04 05:20:53 -0700

    I have no idea what is going on now

--- Helpers/ImageProcessingHelper.cs
+++ Helpers/ImageProcessingHelper.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 using Plugin.Media.Abstractions;
 using Xamarin.Forms;
@@ -33,16 +34,19 @@ namespace NumberTracker
 					};
 					scontent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
 
-					var client = new HttpClient();
-					var multi = new MultipartFormDataContent();
-					multi.Add(scontent);
-					client.BaseAddress = new Uri("http://45.55.137.37:5000/");
-					var response = await client.PostAsync("api/photo", multi);
-					if (response.IsSuccessStatusCode)
+					using (var client = new HttpClient())
 					{
-						var content = await response.Content.ReadAsStringAsync();
-						return content;
-				}
+						var multi = new MultipartFormDataContent();
+						multi.Add(scontent);
+						client.BaseAddress = new Uri("http://45.55.137.37:5000/");
+						var response = await client.PostAsync("api/photo", multi);
+						scontent.Dispose();
+						if (response.IsSuccessStatusCode)
+						{
+							var content = await response.Content.ReadAsStringAsync();
+							return content;
+					}
+					}
 				}
 			}
 			catch (Exception e)
